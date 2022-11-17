b475242607bcd29db08aca8e1a1935956dc04535
hqvjet <viethq.1906@gmail.com>
2022-11-17 19:36:46 +0700

    viet add sign in sign up button on header

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,13 +1,25 @@
 import styles from './Header.module.css'
 
 function Header() {
+
+    const user = JSON.parse(localStorage.getItem('YoleUser'))
+
     return (
         <div className={styles.header}>
             <div
-                className={styles.logoPage}    
+                className={styles.logoPage}
             >
                 Logo
             </div>
+            <div className={styles.btnGroup}>
+                <button className={styles.signin} onClick={() => window.location.href = '/sign-in'}>
+                    Sign in
+                </button>
+                <button className={styles.signup} onClick={() => window.location.href = '/sign-up'}>
+                    Sign up
+                </button>
+            </div>
+
         </div>
     )
 }
