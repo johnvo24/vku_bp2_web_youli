df49b7df70ec9dc2c1041e43a14b80299449dfb2
hqvjet <viethq.1906@gmail.com>
2022-11-18 00:34:09 +0700

    vjet update shopping UI

--- client/src/Components/BudgetComponents/AddItem/index.js
+++ client/src/Components/BudgetComponents/AddItem/index.js
@@ -16,7 +16,8 @@ export default function AddItem(props) {
                 <Container customStyles={{
                     border: '1px solid red',
                     display: 'flex',
-                    flexDirection: 'column'
+                    flexDirection: 'column',
+                    alignItems: 'center'
                 }}>
                     <div className={styles.header}>{CONTENT.itemHeader[data[1]]}</div>
                     <div className={styles.informationContainer}>
@@ -25,18 +26,20 @@ export default function AddItem(props) {
                         <label className={styles.label} htmlFor='price'>{CONTENT.itemPrice[data[1]]}</label>
                         <input type='text' id='price' className={styles.input}/>
                         <label className={styles.label} htmlFor='time'>{CONTENT.purchaseTime[data[1]]}</label>
-                        <input type='text' id='time' className={styles.input}/>
+                        <input type='date' id='time' className={styles.input}/>
                         <label className={styles.label} htmlFor='classify'>{CONTENT.itemClassify[data[1]]}</label>
-                        <input type='text' id='classify' className={styles.input}/>
+                        <select id='classify' className={styles.input}>
+                            
+                        </select>
                     </div>
                     <div className={styles.btnGroup}>
-                        <button className={styles} onClick={
+                        <button className={styles.btn} onClick={
                             () => {
                                 props.itemTheme(false)
                                 props.mainTheme(true)
                             }}>{GCONTENT.back[data[1]]}</button>
-                        <button className={styles}>{GCONTENT.reset[data[1]]}</button>
-                        <button className={styles}>{GCONTENT.save[data[1]]}</button>
+                        <button className={styles.btn}>{GCONTENT.reset[data[1]]}</button>
+                        <button className={styles.btn}>{GCONTENT.save[data[1]]}</button>
                     </div>
                 </Container>
             </div>
